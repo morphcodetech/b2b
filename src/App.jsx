aab8dfa1af1c5d8fbe4122a549b0c7e00fd9c74e
Sosang <sosang7712@gmail.com>
2026-02-18 21:49:01 +0545

    Initial commit: Added Contact page with Map, Partners, and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { Routes, Route } from "react-router-dom"; 
-import Home from "./pages/Home"; 
-import About from "./pages/About"; 
-import Contact from "./pages/Contact"; 
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import MainLayout from "./layouts/MainLayout";
+import SimpleLayout from "./layouts/SimpleLayout";
 
-function App() { 
-  return ( 
-    <Routes> 
-      <Route path="/" element={<MainLayout />}>
-        {/* Home page with all the property sections */}
-        <Route index element={<Home />} /> 
-        <Route path="about" element={<About />} /> 
-        <Route path="contact" element={<Contact />} /> 
+function App() {
+  return (
+    <Routes>
+
+      {/* Default route → show Contact first */}
+      <Route path="/" element={<Navigate to="/contact" />} />
+
+      {/* Pages WITH Navbar */}
+      <Route element={<MainLayout />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Route>
-    </Routes> 
+
+      {/* Page WITHOUT Navbar */}
+      <Route element={<SimpleLayout />}>
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+
+    </Routes>
   );
-} 
+}
 
 export default App;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,14 @@
+// MainLayout.jsx
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar"; // FIXED: Changed from ../components/Navbar to ./Navbar
+import Navbar from "./Navbar";
+
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Navbar />
-      <main className="flex-grow">
-        <Outlet /> 
-      </main>
-     
-    </div>
+      <Outlet />
+    </>
   );
 };
 
