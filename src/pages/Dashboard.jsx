bd636a8ac7bd21e247c5fabca801199952b211cc
sk9o9 <sumitkhadka098@gmail.com>
2026-03-03 18:24:13 +0545

    heading section completed

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "../components/Heading/Heading";
+
+const Dashboard = () => {
+  return (
+    <div className="min-h-screen ">
+      {/* No props needed now! */}
+      <Heading />
+    </div>
+  );
+};
+
+export default Dashboard;
