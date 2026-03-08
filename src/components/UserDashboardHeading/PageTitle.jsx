9fce0852369f6a81ccabc15f1ecf427881994216
Alish Bajracharya <135405911+Alish-Bajracharya@users.noreply.github.com>
2026-03-08 22:40:39 +0545

    clear main branch

--- src/components/UserDashboardHeading/PageTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const PageTitle = ({ title }) => {
-  return (
-    <h1 className="text-xl font-bold text-gray-800 tracking-tight">{title}</h1>
-  );
-};
-
-export default PageTitle;
