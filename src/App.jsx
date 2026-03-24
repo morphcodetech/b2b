7482ce97e373dbba6e26d4bb887290f10e8752a7
sk9o9 <sumitkhadka098@gmail.com>
2026-03-24 20:30:04 +0545

    Added images and categories data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import CategoryScrollSection from "./components/CategoryScrollSection/CategoryScrollSection";
+import CategoryGridSection from "./components/CategoryGridSection/CategoryGridSection";
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <div className="min-h-screen bg-white">
+      {/* Horizontal Scroll Strip */}
+      <CategoryScrollSection />
+
+      {/* Static Grid Section */}
+      <CategoryGridSection />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/data/categoriesData.js
@@ -0,0 +1,98 @@
+import img1 from "../assets/categoryimages/img1.png";
+import img2 from "../assets/categoryimages/img2.png";
+import img3 from "../assets/categoryimages/img3.png";
+import img4 from "../assets/categoryimages/img4.png";
+import img5 from "../assets/categoryimages/img5.png";
+import img6 from "../assets/categoryimages/img6.png";
+import img7 from "../assets/categoryimages/img7.png";
+import img8 from "../assets/categoryimages/img8.png";
+import img9 from "../assets/categoryimages/img9.png";
+import img10 from "../assets/categoryimages/img10.png";
+import img12 from "../assets/categoryimages/img12.png";
+
+export const categories = [
+  {
+    id: 1,
+    name: "Fish & Meats",
+    count: "125+ Products",
+    image: img1,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 2,
+    name: "Dairy & Eggs",
+    count: "125+ Products",
+    image: img2,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 3,
+    name: "Snacks",
+    count: "125+ Products",
+    image: img3,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 4,
+    name: "Frozen Foods",
+    count: "125+ Products",
+    image: img4,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 5,
+    name: "Vegetables",
+    count: "125+ Products",
+    image: img5,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 6,
+    name: "Fish & Meats",
+    count: "125+ Products",
+    image: img6,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 7,
+    name: "Desserts",
+    count: "125+ Products",
+    image: img7,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 8,
+    name: "Drinks & Juice",
+    count: "125+ Products",
+    image: img8,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 9,
+    name: "Animals Food",
+    count: "125+ Products",
+    image: img9,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 10,
+    name: "Fresh Fruits",
+    count: "125+ Products",
+    image: img10,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 11,
+    name: "Bakery",
+    count: "125+ Products",
+    image: img12,
+    bg: "#E8F5E9",
+  },
+  {
+    id: 12,
+    name: "Beverages",
+    count: "125+ Products",
+    image: img2,
+    bg: "#E8F5E9",
+  },
+];
